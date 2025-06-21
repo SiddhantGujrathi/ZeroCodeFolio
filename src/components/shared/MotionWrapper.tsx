4e5b9821a5e5d42aef53f6d754688d35fcccf98d
Siddhant Gujrathi <gujrathisiddhant@gmail.com>
2025-06-21 12:36:18 +0000

    make it standard portfolio so and more professional

--- src/components/shared/MotionWrapper.tsx
+++ src/components/shared/MotionWrapper.tsx
@@ -7,10 +7,10 @@ export function MotionWrapper({ children, className, id }: { children: React.Rea
     <motion.section
       id={id}
       className={className}
-      initial={{ opacity: 0, x: 50 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      viewport={{ once: true, amount: 0.1 }}
-      transition={{ duration: 0.7, ease: "easeOut" }}
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
     >
       {children}
     </motion.section>
